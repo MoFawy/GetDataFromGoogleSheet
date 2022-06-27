16f1379593fecaac43638ed30b1cc3ef1833afc8
MohamedFawy <MohamedFawy@MOHAMEDABDELBAS>
2022-06-27 11:52:50 +0200

    Add project files.

---
+++ Forms/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="GetDataFromGoogleSheet.Forms.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Asset" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="Asset" HeaderText="Asset" ReadOnly="True" SortExpression="Asset"></asp:BoundField>
+                    <asp:BoundField DataField="Asset Name" HeaderText="Asset Name" SortExpression="Asset Name"></asp:BoundField>
+                    <asp:BoundField DataField="Model" HeaderText="Model" SortExpression="Model"></asp:BoundField>
+                    <asp:BoundField DataField="Vendor" HeaderText="Vendor" SortExpression="Vendor"></asp:BoundField>
+                    <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description"></asp:BoundField>
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString='<%$ ConnectionStrings:DBspreadsheetConnectionString %>' SelectCommand="SELECT * FROM [Asset]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Forms/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GetDataFromGoogleSheet.Forms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static readonly string ApplicationName = "Google Sheets API .NET Quickstart"; 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String spreadsheetId = "spreadsheetId";
+            String range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Import Data From a sample spreadsheet To Sqlserver :
+            ValueRange response = request.Execute(); IList<IList<Object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                using (SqlConnection con = new SqlConnection("data source=MOHAMEDABDELBAS;initial catalog=DBspreadsheet;integrated security=True"))
+                {
+                    foreach (var row in values)
+                    {
+                        // Insert columns A and E, which correspond to indices 0 and 4.
+                        SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Asset]([Asset],[Asset Name],[Model],[Vendor],[Description]) " +
+                            "VALUES(" + Convert.ToInt32(row[0]) + "," + row[1] + "," + row[2] + "," + row[3] + "," + row[4] + ")", con);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
